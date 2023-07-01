046a76da16bb2622f71f12cee70c0b336302f521
Eliazer <adetunjieliazer@gmail.com>
2023-06-30 18:47:03 -0700

    Facebook Webpage

---
+++ scripts/responses.js
@@ -0,0 +1,58 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "Sunnybot") {
+        return "I'm here!";
+    } else if (input == "sunnybot") {
+        return "I'm here!";
+    } else if (input == "Heart clicked!") {
+        return "Thank you!";
+    } if (input == "Hello") {
+        return "Hello there!";
+    } else if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "Hi") {
+        return "I'm Sunnybot.🤖";
+    } else if (input == "hi") {
+        return "I'm Sunnybot.🤖";
+    } else if (input == "How are you?") {
+        return "I'm fine. Thanks!";
+    } else if (input == "") {
+        return "Input something!";
+    } 
+    
+    // Greetings responses
+     else if (input == "What is your name?") {
+        return "I'm Sunnybot.🤖";
+    } else if (input == "What's your name?") {
+        return "I'm Sunnybot.🤖";
+    } else if (input == "How old are you?") {
+        return "A year old!";
+    } else if (input == "how old are you?") {
+        return "I'm a year old!";
+    } else if (input == "Who is your father?") {
+        return "I was developed by Eliazer";
+    } else if (input == "Who's your father?") {
+        return "I was developed by Eliazer";
+    } else if (input == "What do you do?") {
+        return "I'm here to asist you! concerning this website.";
+    } else if (input == "What can you do?") {
+        return "I'm here to asist you! concerning this website.";
+    } else if (input == "What is your purpose?") {
+        return "I'm here to asist you! concerning this website.";
+    } else if (input == "What is your favourite colour?") {
+        return "Black!";
+    } else if (input == "What is your favorite color?") {
+        return "Black!";
+    } else if (input == "Who is owner of this website?") {
+        return "Eliezer Sunny!";
+    } else if (input == "Who create this website?") {
+        return "Eliezer Sunny!";
+    } else if (input == "Who owns this website?") {
+        return "Eliezer Sunny!";
+    } else {
+        return "Ooops!😟 Try asking something else. I'm under-development!";
+    }
+    
+    
+  
+}
